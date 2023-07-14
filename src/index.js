082f20a7bbb4e925669696376575376639a4a793
Peace Adegunju <peacemastre@gmail.com>
2023-07-14 04:51:59 +0100

    removing files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+function MainHeader() {
+  return <h1>REACT COURSE</h1>;
+}
+function SubHeader() {
+  return <p>This will be an exciting course</p>;
+}
+function Header() {
+  return (
+    <div>
+      <MainHeader></MainHeader>
+      <SubHeader></SubHeader>
+    </div>
+  );
+}
+function MainBody() {
+  return (
+    <div>
+      <p>In this coursde we will learn react js by building Taskopedia</p>
+      <ul>
+        <li>Call me</li>
+        <li>Go to walmart</li>
+      </ul>
+    </div>
+  );
+}
+
+function Footer() {
+  return <p>Happy Coding</p>;
+}
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <Header />
+    <MainBody />
+    <Footer />
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
