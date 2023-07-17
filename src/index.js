98bf15c1ca609e0b14c4cb1f8e3f8783f8d9b9f5
Peace Adegunju <peacemastre@gmail.com>
2023-07-17 14:37:17 +0100

    made changes to index,js

--- src/index.js
+++ src/index.js
@@ -39,11 +39,19 @@ function MainBody() {
 
 function Students(){
   const fullName = "Kris Walley";
-
+  const programmingExp=2;
   return(
     <div className="container p-4">
-      <div className="row">
-        Students Enrolled
+      <div className="row">Students Enrolled</div>
+      <div className="row border">
+        <div className="col-2">
+          <img src={`https://ui-avatars.com/api/?name=${fullName}`} className="w-100" alt="avatar"></img>
+        </div>
+        <div className="col-10">
+          {fullName}
+          <br/>
+          Programming Experience {programmingExp} years
+        </div>
       </div>
     </div>
   )
